48c35734abde5e511827e2c57693dbc687e8b942
Rogelio <rogelioceballos218@gmail.com>
2024-09-16 20:30:27 -0600

    Actualización en el template html

--- src/domain/templates/email.template.ts
+++ src/domain/templates/email.template.ts
@@ -8,7 +8,7 @@ export function generatemonkeyPoxEmailTemplate(lat: number, lng: number, genre: 
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Detalles del Incidente</title>
+        <title>Detalles del Caso</title>
         <style>
             body {
                 font-family: Arial, sans-serif;
